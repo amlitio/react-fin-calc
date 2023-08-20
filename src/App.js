665976c7a2440e7052fd4fe86217a8d220d95472
kleos <36254493+amlitio@users.noreply.github.com>
2023-08-20 11:06:53 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// App.js
+
+import Calculator from './components/Calculator';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Finance Calculator</h1>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
